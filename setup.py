232eabcef33d1ab17aa74d78b62e91799bed89f0
Gaurav Lalchandani <glalchandanig@gmail.com>
2020-08-30 20:04:53 +0530

    Include non-code files to PyPi dist (wheel/tar)

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-import pathlib
+import os
 from setuptools import setup
 
-HERE   = pathlib.Path(__file__).parent
-README = (HERE / 'README.md').read_text()
+
+README_PATH = os.path.join(os.path.dirname(__file__), 'README.md')
+with open(README_PATH) as f:
+    README_DATA = f.read()
+
 
 # This call to setup() does all the work
 setup(
     name                          = 'yarc-server',
     version                       = '0.1.0',
     description                   = 'Yet Another Remote Control',
-    long_description              = README,
+    long_description              = README_DATA,
     long_description_content_type = 'text/markdown',
     url                           = 'https://github.com/return007/yarc',
     author                        = 'return007',
     author_email                  = 'glalchandanig@gmail.com',
     packages                      = ['yarc'],
     include_package_data          = True,
+    python_requires               = '>=3.6',
     install_requires = [
         'pyautogui',
         'flask',
